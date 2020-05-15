41b7794d45bfd155abb41ed1da689790ec372eae
annaweronica <anna.janiak@yahoo.pl>
2020-05-15 23:02:02 +0000

    profile management.html created

--- agile_app/urls.py
+++ agile_app/urls.py
@@ -6,4 +6,5 @@ from . import views
 urlpatterns = [
     path('', views.get_main_page, name='get_main_page'),
     path('contact/', views.get_contact_page, name='get_contact_page'),
+    path('package_management/', views.get_package_management, name='get_package_management'),
 ]
