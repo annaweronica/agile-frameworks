a742b5fca30c4eeb76610cf7aa3af59414c74ab8
annaweronica <anna.janiak@yahoo.pl>
2020-05-17 20:57:40 +0000

    fixed INSTALLED_APPS in settings.py due to herokuapp error

--- agile/settings.py
+++ agile/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
-    'storages',
     'allauth',
     'allauth.account',
     'allauth.socialaccount',
